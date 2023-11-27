0e5d06ebdc2e41a231a0647cfa4e208c5bf99ad2
Onelio Viera <ojvwebdesign@gmail.com>
2023-11-26 19:34:29 -0500

    update file

--- day-1/script.js
+++ day-1/script.js
@@ -10,23 +10,23 @@ const someOtherNumber = 3;
 const multipliedNumbers = myFavoriteNumber * someOtherNumber;
 
 // UNCOMMENT THE LINE BELOW TO PRINT THE RESULT
-console.log(multipliedNumbers);
+console.log(multipliedNumbers); // 21
 
 // 4/5: Store your first name in a variable and your last name in another
 const firstName = 'Onelio';
 const lastName = 'Viera';
-console.log(firstName, lastName);
+console.log(firstName, lastName); // Onelio Viera
 
 // 6: Store your full name in a variable by adding the two variables
 const fullName = firstName + ' ' + lastName;
-console.log(fullName);
+console.log(fullName); // Onelio Viera
 
 // Bonus: Store your firsName, lastName in a variable called myInfo using template literals
 const myInfo = (`My first name is ${firstName}, and my last name is ${lastName}`);
-console.log(myInfo);
+console.log(myInfo); // My first name is Onelio, and my last name is Viera
 
 // UNCOMMENT THE LINE BELOW TO PRINT THE RESULT
-console.log(fullName);
+console.log(fullName); // Onelio Viera
 
 // 7: Store your firstName, lastName, favColor and hometown in an object called myDetails
 const myDetails = {
@@ -37,19 +37,19 @@ const myDetails = {
 }
 
 // UNCOMMENT THE LINE BELOW TO PRINT THE RESULT
-console.log(myDetails);
+console.log(myDetails); // { firstName: 'Onelio', lastName: 'Viera', favColor: 'purple', hometown: 'Tampa, FL' }
 
 // 8: Using the data from your myDetails variable, create a one sentence bio and store it as a string.
 const myBio = `Hello, my name is ${myDetails.firstName} ${myDetails.lastName}. My current favorite color is ${myDetails.favColor}, and I am from ${myDetails.hometown}.`;
 
 // UNCOMMENT THE LINE BELOW TO PRINT THE RESULT
-console.log(myBio);
+console.log(myBio); // Hello, my name is Onelio Viera. My current favorite color is purple, and I am from Tampa, FL.
 
 // 9: Create an array called dreamDestinations with your top 5 travel destinations.
 const dreamDestinations = ['Cuba', 'Span', 'Italy', 'Portugal', 'Greece'];
 
 // UNCOMMENT THE LINE BELOW TO PRINT THE RESULT
-console.log(dreamDestinations);
+console.log(dreamDestinations); // [ 'Cuba', 'Span', 'Italy', 'Portugal', 'Greece' ]
 
 //--------------------------------------------------
 // JS4B - Homework - Challenge Part 2
@@ -58,7 +58,7 @@ console.log(dreamDestinations);
 // 10: Write a loop that logs 'hello' to the console 10 times
 for (let i = 0; i < 10; i++) {
   // code here
-  console.log('hello');
+  console.log('hello'); // hello
 }
 
 // 11: Use a WHILE loop to add 1 to 'count' 100 times
@@ -68,12 +68,12 @@ while (count < 100) {
   count += 1;
 }
 // Console log the new value of count
-console.log(count);
+console.log(count); // 100
 
 // 12: Print the even numbers between 1 and 100
 for (let i = 0; i <= 100; i++) {
   if (i % 2 === 0) {
-    console.log(i);
+    console.log(i); // 0, 2, 4, 6, 8, 10, 12, 14, 16, 18..., 100
   }
 }
 
@@ -108,3 +108,6 @@ const mySpecialArray = [
 //ex. if you used this loop on the array [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] you would see the text logged to the console "0369"
 
 // loop goes here
+
+
+
